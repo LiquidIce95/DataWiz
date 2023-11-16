8d52b186f079c2138aded95a480a4368068115f6
LiquidIce95 <davidsanchez062@gmail.com>
2023-11-16 15:01:45 +0100

    dd

--- Frontend/src/app/calculator/descriptive/descriptive.component.spec.ts
+++ Frontend/src/app/calculator/descriptive/descriptive.component.spec.ts
@@ -6,6 +6,8 @@ import { FormsModule } from '@angular/forms';
 fdescribe('DescriptiveComponent', () => {
   let component: DescriptiveComponent;
   let fixture: ComponentFixture<DescriptiveComponent>;
+  let service: TableDataService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [DescriptiveComponent],
@@ -17,6 +19,25 @@ fdescribe('DescriptiveComponent', () => {
     fixture = TestBed.createComponent(DescriptiveComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.tableDataService['tableData']=[
+      { 'name': 'John', 'age': 30, 'income': 0 },
+      { 'name': 'Alice', 'age': 25, 'income': 100 }
+    ]
+
+    component.tableDataService['tableHeaders'] = {
+      'name':['nominal',false],
+      'age' :['metric',false],
+      'income' : ['metric',false]
+    };
+
+    component.tableDataService['tableKeys'] = [
+      'name',
+      'age',
+      'income'
+    ];
+
+    component.averages = {};
+    
   });
   
 
@@ -24,6 +45,81 @@ fdescribe('DescriptiveComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  describe('getAverages()',()=>{
+    it('compute average of income',()=>{
+      component.tableDataService['tableHeaders']['income'][1] = true;
+
+      component.getAverages();
+
+      let result = component.averages;
+
+      expect(result['income']).toEqual(50);
+    });
+
+    it('check if none is computed',()=>{
+      
+      component.getAverages();
+
+      let result = component.averages;
+
+      expect(result).toEqual({});
+    });
+    
+
+    it('check if non metric are not computed'),()=>{
+      component.tableDataService['tableData']=[
+        { 'name': 'John', 'age': 30, 'income': 0 },
+        { 'name': 'Alice', 'age': 25, 'income': 100 }
+      ]
+  
+      component.tableDataService['tableHeaders'] = {
+        'name':['nominal',false],
+        'age' :['nominal',true],
+        'income' : ['nominal',true]
+      };
+  
+      component.tableDataService['tableKeys'] = [
+        'name',
+        'age',
+        'income'
+      ];
+
+      component.getAverages();
+
+      let result = component.averages;
+
+      expect(result).toEqual({});
+    }
+  });
+
+  describe('getAverageKeys()',()=>{
+
+    it('two keys with averages',()=>{
+      component.averages = {'age' : 15, 'income':50};
+
+      component.tableDataService['tableData']=[
+        { 'name': 'John', 'age': 30, 'income': 0 },
+        { 'name': 'Alice', 'age': 0, 'income': 100 }
+      ]
+  
+      component.tableDataService['tableHeaders'] = {
+        'name':['nominal',false],
+        'age' :['metric',true],
+        'income' : ['metric',true]
+      };
+  
+      component.tableDataService['tableKeys'] = [
+        'name',
+        'age',
+        'income'
+      ];
+
+      let result = component.getAverageKeys();
+  
+      expect(result).toEqual(['age','income']);
+    });
+    
+  });
   
   
 
